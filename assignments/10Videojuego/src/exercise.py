8ba7f04d3ff10cdd00d903247410a57436f066f3
A01411592 <89592174+A01411592@users.noreply.github.com>
2021-08-30 10:14:46 -0500

    ejercicio videojuego

--- assignments/10Videojuego/src/exercise.py
+++ assignments/10Videojuego/src/exercise.py
@@ -1,6 +1,12 @@
 def main():
     # escribe tu código abajo de esta línea
     pass
+    nuevo = float(input("Dame la cantidad de juegos nuevos: "))
+    usado = float(input("Dame la catidad de juegos usados: "))
+    
+    costo = (nuevo * 1000) + (usado * 350)
+    
+    print("El total de la compra es:",costo)
 
 
 if __name__ == '__main__':
