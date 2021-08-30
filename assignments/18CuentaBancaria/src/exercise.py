67e033e85dba4d1ada26a74a2a27c1a9524d193b
A01411592 <89592174+A01411592@users.noreply.github.com>
2021-08-30 10:15:29 -0500

    ejercicio banco

--- assignments/18CuentaBancaria/src/exercise.py
+++ assignments/18CuentaBancaria/src/exercise.py
@@ -1,6 +1,15 @@
 def main():
     # escribe tu código abajo de esta línea
     pass
+    saldo = float(input("Dame el saldo del mes anterior: "))
+    ingreso = float(input("Dame los ingresos: "))
+    egreso = float(input("Dame los egresos: "))
+    cheques = float(input("Dame el número de cheques: "))
+    pagar = saldo + ingreso - egreso - (cheques * 13)
+    interes = (pagar * 7.5) / 100
+    si = pagar - interes 
+
+    print("El saldo final de la cuenta es:",si)
 
 
 if __name__ == '__main__':
